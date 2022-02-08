f446d9a91dfb66e67d4e43386d3913ccb0711b59
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-02-08 12:04:25 -0600

    Add function for pagination

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let option = `quotes`;
 let searchAnime = false;
 let searchChar = false;
+let pageNum = 1;
 
 document.addEventListener('DOMContentLoaded', () => {
     //fetchAnimeQuotes(option); 
@@ -27,6 +28,9 @@ function loadAnimeQuotes(quoteObj){
     document.getElementById('anime-list').textContent = '';
     
     if(option.includes('quotes')){
+        if(option.includes(pageNum)){
+            pageNav();
+        }
         for(let key in quoteObj){
             let li = makeQuote(quoteObj[key]);
             quoteList.appendChild(li);
@@ -97,6 +101,37 @@ function formToggle(form, bool){
     return bool;
 }
 
+//used from Monsters lab
+function pageNav(animeTitle){
+    const backBtn = document.getElementById('back');
+    const forwardBtn = document.getElementById('forward');
+
+    backBtn.style.display = "block";
+    forwardBtn.style.display = "block";
+
+    backBtn.addEventListener('click', function(){
+        if(pageNum > 1){
+            pageNum--;
+            if(option.includes('anime')){
+                option = `quotes/anime?title=${animeTitle.toLowerCase()}&page=${pageNum}`; 
+            }
+            else{
+                option = `quotes/character?name=${charName.toLowerCase()}&page=${pageNum}`;
+            }
+            fetchAnimeQuotes(option);  
+        }
+        else{
+            alert('Can\'t go back anymore');
+        }
+    });
+
+    forwardBtn.addEventListener('click', function(){
+        pageNum++;
+        fetchAnimeQuotes(option);
+    })
+
+}
+
 function initializeFormsAndBtns(){
 
     const searchAnimeBtn = document.getElementById('search-anime-btn');
@@ -107,10 +142,16 @@ function initializeFormsAndBtns(){
 
     const tenQuotesBtn = document.getElementById('ten-random-quotes');
     const randomQuoteBtn = document.getElementById('random-quote');
-    const availableAnimeBtn = document.getElementById('available-anime')
+    const availableAnimeBtn = document.getElementById('available-anime');
+
+    const backBtn = document.getElementById('back');
+    const forwardBtn = document.getElementById('forward');
 
     searchAnimeForm.style.display = "none";
     searchCharForm.style.display = "none";
+
+    backBtn.style.display = "none";
+    forwardBtn.style.display = "none";
 
     searchAnimeBtn.addEventListener('click', () => {
         searchAnime = formToggle(searchAnimeForm, searchAnime);
@@ -138,14 +179,14 @@ function initializeFormsAndBtns(){
     searchAnimeForm.addEventListener('submit', e => {
         e.preventDefault();
         let animeTitle = Array.from(document.getElementsByClassName('input-text'))[0].value;
-        option = `quotes/anime?title=${animeTitle.toLowerCase()}`
+        option = `quotes/anime?title=${animeTitle.toLowerCase()}&page=${pageNum}`;
         fetchAnimeQuotes(option);
     });
 
     searchCharForm.addEventListener('submit', e => {
         e.preventDefault();
         let charName = Array.from(document.getElementsByClassName('input-text'))[1].value;
-        option = `quotes/character?name=${charName.toLowerCase()}`;
+        option = `quotes/character?name=${charName.toLowerCase()}&page=${pageNum}`;
         fetchAnimeQuotes(option);
     })
 }
